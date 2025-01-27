83baa96903c5f8ccede9ca94023326db65df5589
Eddan <edvin.runhellen1@gmail.com>
2025-01-27 13:42:56 +0100

    Added simulation of API req delay and loading state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import viteLogo from '/vite.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Input from './input';
 import List from './List';
 
@@ -8,8 +8,25 @@ import List from './List';
 function App() {
 	const [todo, setTodo] = useState('');
 	const [todos, setTodos] = useState([]);
+	const [loading, setLoading] = useState(true)
+
+
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/todos')
+			.then((res) => res.json())
+			.then((data) => {
+				const uncompletedTodos = data.filter(todo => !todo.complete)
+					.map(todo => todo.title) 
+				setTimeout(() => {
+					setTodos(uncompletedTodos)
+					setLoading(false)
+				}, 2000)
+			
+		})
+	}, [])
 
 	const addTodo = () => {
+		console.log(todos)
 		if (todo !== "")
 		setTodos([...todos, todo])
 		setTodo("")
@@ -20,11 +37,13 @@ function App() {
 		setTodos(uncompletedTodos)
 	}
 
+	
+
 	return (
 		<div className="App">
 			<img className="logo" src="/logo.png" alt="Tasktail" />
 			<Input setTodo={setTodo} todo={todo} addTodo={addTodo} />
-			<List todos={todos} complete={complete} />
+			{loading ? <h1>Loading</h1> : <List todos={todos} complete={complete} />}			
 		</div>
 	);
 }
